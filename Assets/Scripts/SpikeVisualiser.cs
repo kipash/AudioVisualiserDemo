3de42c5fbea3828787e30ef8be2088be1c75a88e
Krystof <kipash612@gmail.com>
2025-03-30 00:16:07 +0100

    spiky mesh ; progress

---
+++ Assets/Scripts/SpikeVisualiser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class SpikeVisualiser : Visualiser
+{
+    public MeshRenderer MeshRenderer;
+    public float Smoothing = 1;
+
+    float _controlTarget;
+    float _control;
+    public override void OnGateTriggered(int gateScore)
+    {
+        _controlTarget = Mathf.Clamp01(gateScore / GateScoreLimit.y);
+    }
+
+    private void Update()
+    {
+        _control = Mathf.Lerp(_control, _controlTarget, Time.deltaTime * Smoothing);
+        MeshRenderer.material.SetFloat("_CONTROL", _control);
+    }
+}
